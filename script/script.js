7534b21cbc799e669ea2eaed68b2b8d33175d9f1
Julia Chelnokova <chelnokova.yuv@yandex.ru>
2023-02-21 15:35:50 +0700

    feat: доработка js попапа

---
+++ script/script.js
@@ -0,0 +1,34 @@
+let editBtn = document.querySelector(".profile__btn-edit");
+let popup = document.querySelector(".popup");
+let closeBtn = document.querySelector(".popup__btn-close");
+
+function openPopup() {
+	popup.classList.add('popup_opened');
+}
+editBtn.addEventListener('click', openPopup);
+
+function closePopup() {
+	popup.classList.remove('popup_opened');
+}
+closeBtn.addEventListener('click', closePopup);
+
+let formElement = document.querySelector(".popup__form");
+let nameInput = document.querySelector(".popup__name");
+let jobInput = document.querySelector(".popup__job");
+
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+
+	nameInput.getAttribute('value');
+	jobInput.getAttribute('value');
+
+	let nameProfile = document.querySelector('.profile__name');
+	let jobProfile = document.querySelector('.profile__job');
+
+	nameProfile.textContent = nameInput.value;
+	jobProfile.textContent = jobInput.value;
+
+	closePopup();
+}
+
+formElement.addEventListener('submit', handleFormSubmit);

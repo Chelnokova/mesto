5518e5fd79a72f2eed4d0b66d9c243545a0d9c19
Julia Chelnokova <chelnokova.yuv@yandex.ru>
2023-02-26 14:35:11 +0700

    feat: карточки заполняются js

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,38 @@ let jobInput = document.querySelector('.popup__input_type_job');
 let nameProfile = document.querySelector('.profile__name');
 let jobProfile = document.querySelector('.profile__job');
 
+const cardContainer = document.querySelector('.elements');
+const cardTemplate = document.querySelector('#template__card').content;
+
+
+
+const initialCards = [
+	{
+	  name: 'Архыз',
+	  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+	},
+	{
+	  name: 'Челябинская область',
+	  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+	},
+	{
+	  name: 'Иваново',
+	  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+	},
+	{
+	  name: 'Камчатка',
+	  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+	},
+	{
+	  name: 'Холмогорский район',
+	  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+	},
+	{
+	  name: 'Байкал',
+	  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+	}
+  ];
+
 function openPopup() {
 	popup.classList.add('popup_opened');
 	nameInput.value = nameProfile.textContent;
@@ -30,3 +62,16 @@ function handleFormSubmit (evt) {
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
+
+initialCards.forEach(function fillCard(element) {
+	
+	const cardElement = cardTemplate.cloneNode(true);
+
+	cardElement.querySelector('.elements__img').src = element.link;
+
+	cardElement.querySelector('.elements__img').alt = `На фото ${element.name}`;
+
+	cardElement.querySelector('.elements__title').textContent = element.name;
+
+	cardContainer.append(cardElement);
+});

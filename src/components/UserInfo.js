3414c0d295d1adb2f722983a46f3c0ff8ab74a6c
Julia Chelnokova <chelnokova.yuv@yandex.ru>
2023-05-09 21:24:04 +0700

    feat: изменение UserInfo

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,16 +1,21 @@
 export class UserInfo {
-	constructor({nameSelector, jobSelector}){
+	constructor({nameSelector, jobSelector, avatarSeleector}){
 		this._name = document.querySelector(nameSelector);
 		this._job = document.querySelector(jobSelector);
+		this._avatar = document.querySelector(avatarSeleector);
+		this._userId = ''
 	}
 	getUserInfo() {
 		return {
 			name: this._name.textContent,
-			job: this._job.textContent
+			job: this._job.textContent,
+			avatar: this._avatar.link
 		}
 	}
-	setUserInfo(name, job) {
+	setUserInfo(name, about, avatar, _id) {
 		this._name.textContent = name;
-		this._job.textContent = job;
+		this._job.textContent = about;
+		this._avatar.src = avatar;
+		this._userId = _id;
 	}
 }
